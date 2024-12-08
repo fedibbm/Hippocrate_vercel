e3541b6117e5cfc2d708d31ee3c6519f28b02c33
fedibbm <benbrahimfedi4@gmail.com>
2024-12-08 13:13:04 +0100

    mlkjmlkj

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,96 +2,125 @@ import { useState, useEffect } from "react";
 import logo from "../assets/logo.png";
 import menu from "../assets/menu.svg";
 import close from "../assets/close.svg";
-import { Link } from "react-router-dom";
+import { IoMdClose, IoMdMenu } from "react-icons/io";
+
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isVisible, setIsVisible] = useState(true);
-  const [lastScrollY, setLastScrollY] = useState(0);
+    const location = useLocation();
+    const [isOpen, setIsOpen] = useState(false);
+    const [isVisible, setIsVisible] = useState(true);
+    const [lastScrollY, setLastScrollY] = useState(0);
+    const [isSolid, setIsSolid] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollY = window.scrollY;
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollY = window.scrollY;
 
-      if (currentScrollY > lastScrollY) {
-        setIsVisible(false);
-      } else {
-        setIsVisible(true);
-      }
-      setLastScrollY(currentScrollY);
-    };
+            if (currentScrollY > lastScrollY) {
+                setIsVisible(false);
+            } else {
+                setIsVisible(true);
+            }
+            setLastScrollY(currentScrollY);
+            if (currentScrollY > window.innerHeight * 0.8) {
+                setIsSolid(true);
+            } else {
+                setIsSolid(false);
+            }
+        };
 
-    window.addEventListener("scroll", handleScroll);
+        window.addEventListener("scroll", handleScroll);
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [lastScrollY]);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, [lastScrollY]);
 
-  return (
-    <nav
-      className={`w-full h-[77px] fixed top-0 z-50 flex items-center justify-between px-6 md:px-14 transition-transform duration-300 ${
-        isVisible ? "translate-y-0" : "-translate-y-full"
-      } bg-gray-400`}
-    >
-      <div className="flex items-center">
-        <img className="w-[40px] h-[50px] mr-4" src={logo} alt="Logo" />
-        <Link to="/" className="text-white text-[24px] font-semibold font-['Poppins']">
-          Hippocrate
-        </Link>
-      </div>
-      <div className="hidden md:flex space-x-8">
-        <Link
-          to="/"
-          className="mt-2 text-white text-sm font-bold font-['Poppins'] tracking-tight hover:text-[#1f8287]"
+    return (
+        <nav
+            className={`w-full h-[77px] fixed top-0 z-50 flex items-center justify-between px-6 md:px-14 transition-transform duration-300 ${
+                isVisible ? "translate-y-0" : "-translate-y-full"
+            } ${isSolid ? "bg-primary shadow-lg text-black" : ""}`}
         >
-          Accueil
-        </Link>
-        <Link
-          to="/services"
-          className="mt-2 text-[#e6e6e6] text-sm font-normal font-['Poppins'] tracking-tight hover:text-[#1f8287]"
-        >
-          Services
-        </Link>
-        <Link
-          to="/contact"
-          className="text-white text-sm font-semibold font-['Poppins'] tracking-tight bg-[#1f8287] px-4 py-2 hover:text-[#1f8287] hover:bg-white"
-        >
-          Contact
-        </Link>
-      </div>
-      <div className="md:hidden">
-        <button
-          onClick={() => setIsOpen(!isOpen)}
-          className="focus:outline-none"
-        >
-          <img src={isOpen ? close : menu} alt="Menu" className="w-3 h-3" />
-        </button>
-      </div>
-      {isOpen && (
-        <div className="absolute top-[77px] right-0 w-full flex flex-col items-center space-y-4 py-4 md:hidden">
-          <Link
-            to="/"
-            className="text-white text-sm font-bold font-['Poppins'] tracking-tight hover:text-[#1f8287]"
-          >
-            Accueil
-          </Link>
-          <Link
-            to="/services"
-            className="text-[#e6e6e6] text-sm font-normal font-['Poppins'] tracking-tight hover:text-[#1f8287]"
-          >
-            Services
-          </Link>
-          <Link
-            to="/contact"
-            className="text-white text-sm font-semibold font-['Poppins'] tracking-tight bg-[#1f8287] px-4 py-2 hover:text-[#1f8287] hover:bg-white"
-          >
-            Contact
-          </Link>
-        </div>
-      )}
-    </nav>
-  );
+            <div className="flex items-center">
+                <img className="w-[40px] h-[50px] mr-4" src={logo} alt="Logo" />
+                <Link
+                    to="/"
+                    className="text-white text-[24px] font-semibold font-['Poppins']"
+                >
+                    Hippocrate
+                </Link>
+            </div>
+            <div className="hidden md:flex items-center space-x-8">
+                <Link
+                    to="/"
+                    className={`mt-2 md:mt-0 text-white text-sm  font-['Poppins'] tracking-tight hover:text-[#1f8287] ${location.pathname=='/'? "font-bold":"font-normal"}`}
+                >
+                    Accueil
+                </Link>
+                <Link
+                    to="/services"
+                    className={`mt-2 md:mt-0 text-[#e6e6e6] text-sm  font-['Poppins'] tracking-tight hover:text-[#1f8287] ${location.pathname=='/services'? "font-bold":"font-normal"}`}
+                >
+                    Services
+                </Link>
+                <Link
+                    to="/contact"
+                    className={`mt-2 md:mt-0 text-[#e6e6e6] text-sm  font-['Poppins'] tracking-tight hover:text-[#1f8287] ${location.pathname=='/contact'? "font-bold":"font-normal"}`}
+                >
+                    Contact
+                </Link>
+            </div>
+            <div className="md:hidden">
+                <button
+                    onClick={() => setIsOpen(true)}
+                    className="focus:outline-none"
+                >
+                    {/* <img
+                        src={isOpen ? close : menu}
+                        alt="Menu"
+                        className="w-3 h-3"
+                    /> */}
+                    <IoMdMenu className="text-white text-3xl" />
+                </button>
+            </div>
+
+            <div
+                className={`absolute top-0 bg-white h-screen flex justify-start text-black flex-col w-3/4 items-center py-20 md:hidden trnasition-all ease-out duration-300 ${
+                    isOpen ? "right-0" : "-right-[100vw]"
+                }`}
+            >
+                <button className="focus:outline-none">
+                    <IoMdClose
+                        className="text-black text-3xl absolute top-4 right-4"
+                        onClick={() => setIsOpen(false)}
+                    />
+                </button>
+                <Link
+                    to="/"
+                    className={`text-center w-full py-4 border-b  border-gray-300 text-sm  font-['Poppins'] tracking-tight hover:text-[#1f8287] ${location.pathname=='/'? "text-primary font-bold":"text-black"} `}
+                    onClick={() => setIsOpen(false)}
+                >
+                    Accueil
+                </Link>
+                <Link
+                    to="/services"
+                    className={`text-center w-full py-4 border-b  border-gray-300 text-sm  font-['Poppins'] tracking-tight hover:text-[#1f8287] ${location.pathname=='/services'? "text-primary font-bold":"text-black"}`}
+                    onClick={() => setIsOpen(false)}
+                >
+                    Services
+                </Link>
+                <Link
+                    to="/contact"
+                    className={`text-center w-full py-4 border-b   border-gray-300 text-sm  font-['Poppins'] tracking-tight hover:text-[#1f8287] ${location.pathname=='/contact'? "text-primary font-bold":"text-black"}`}
+                    onClick={() => setIsOpen(false)}
+                >
+                    Contact
+                </Link>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,5 +1,4 @@
 import PropTypes from "prop-types";
-import { useState } from "react";
 
 export const ServiceCard = ({ obj }) => {
   const { title, content, number, img } = obj;
